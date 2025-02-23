6bdac9b2735a7ae9aab55df878babd7728c026a5
Cappillen Lee <cappillen@ucsb.edu>
2025-02-22 19:19:05 -0800

    fetch tidal data

--- src/utils/song_stats.py
+++ src/utils/song_stats.py
@@ -40,5 +40,19 @@ def get_tiktok_series(song_id: str):
         last_90_data = parsed_data['chart']['seriesData'][0]['data'][-90:]
         return track_name, last_90_data
 
+def get_tidal_series(song_id: str):
+    res = requests.get(f"https://data.songstats.com/api/v1/analytics_track/{song_id}/top?source=tiktok")
+
+    if res.status_code != 200:
+        return None
+    
+    parsed_data = res.json()
+
+    if parsed_data['result'] == 'success':
+        track_name = parsed_data['trackInfo']['trackName']
+        last_90_data = parsed_data['chart']['seriesData'][0]['data'][-90:]
+        return track_name, last_90_data
+
+
 # print(get_spotify_playlist_series(song_id='njtwgzci'))
 print(get_spotify_reach_series(song_id='njtwgzci'))
