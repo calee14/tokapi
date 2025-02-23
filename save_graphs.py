9c2c194fe4df0b6999d22bf0b17ea9cc8c21ea7b
Ryan Shi <ryanshi66@gmail.com>
2025-02-23 11:17:54 -0800

    datsets

--- save_graphs.py
+++ save_graphs.py
@@ -13,8 +13,10 @@ def get_spotify_playlist_series(song_id: str):
 
     if parsed_data['result'] == 'success':
         track_name = parsed_data['trackInfo']['trackName']
+        artist_name = parsed_data['trackInfo']['artistName']
+        avatar = parsed_data['trackInfo']['avatar']
         last_90_data = parsed_data['chart']['seriesData'][0]['data'][-90:]
-        return track_name, last_90_data
+        return track_name, last_90_data, artist_name, avatar
 
 def get_spotify_reach_series(song_id: str):
     res = requests.get(f"https://data.songstats.com/api/v1/analytics_track/{song_id}/top?source=spotify")
@@ -26,8 +28,10 @@ def get_spotify_reach_series(song_id: str):
 
     if parsed_data['result'] == 'success':
         track_name = parsed_data['trackInfo']['trackName']
+        artist_name = parsed_data['trackInfo']['artistName']
+        avatar = parsed_data['trackInfo']['avatar']
         last_90_data = parsed_data['chart']['seriesData'][1]['data'][-90:]
-        return track_name, last_90_data
+        return track_name, last_90_data, artist_name, avatar
     
 def get_tiktok_series(song_id: str):
     res = requests.get(f"https://data.songstats.com/api/v1/analytics_track/{song_id}/top?source=tiktok")
@@ -39,8 +43,10 @@ def get_tiktok_series(song_id: str):
 
     if parsed_data['result'] == 'success':
         track_name = parsed_data['trackInfo']['trackName']
+        artist_name = parsed_data['trackInfo']['artistName']
+        avatar = parsed_data['trackInfo']['avatar']
         last_90_data = parsed_data['chart']['seriesData'][0]['data'][-90:]
-        return track_name, last_90_data
+        return track_name, last_90_data, artist_name, avatar
 
 def write_csv(filename: str, data, header=["Timestamp", "Value"]):
     with open(filename, "w", newline="") as csvfile:
@@ -89,12 +95,19 @@ def main():
             continue
         print(f"Processing song ID: {code}")
         series = get_spotify_reach_series(code)[1]
+        name = get_spotify_reach_series(code)[0]
+        artist = get_spotify_reach_series(code)[2]
         if series is None:
             print(f"No data found for {code}.")
             continue
         if len(series) > 0:
             filename = f"spotify_reach_series_{code}.csv"
-            write_csv(filename, series)
+          # write_csv(filename, series)
+            with open(filename, "w", newline="") as file:
+                file.write(name + "\n")
+                file.write(artist + "\n")
+                writer = csv.writer(file)
+                writer.writerows(series)
             print(f"Saved Spotify Reach data for {code} to {filename}")
         else:
             print(f"Not enough series available for song ID {code}.")
@@ -102,4 +115,69 @@ def main():
 if __name__ == "__main__":
     main()
 '''
+
+# print(get_spotify_reach_series('n5i60mse'))
+
+'''
+def main():
+    with open("src/utils/songs", "r") as file:
+        codes = file.read().splitlines()
+
+    for code in codes:
+        if code == '49gk6oeq':
+            continue
+        print(f"Processing song ID: {code}")
+        series = get_spotify_playlist_series(code)[1]
+        name = get_spotify_playlist_series(code)[0]
+        artist = get_spotify_playlist_series(code)[2]
+        if series is None:
+            print(f"No data found for {code}.")
+            continue
+        if len(series) > 0:
+            filename = f"spotify_playlist_series_{code}.csv"
+          # write_csv(filename, series)
+            with open(filename, "w", newline="") as file:
+                file.write(name + "\n")
+                file.write(artist + "\n")
+                writer = csv.writer(file)
+                writer.writerows(series)
+            print(f"Saved Spotify Playlist data for {code} to {filename}")
+        else:
+            print(f"Not enough series available for song ID {code}.")
+
+if __name__ == "__main__":
+    main()
+'''
+
+
+def main():
+    with open("src/utils/songs", "r") as file:
+        codes = file.read().splitlines()
+
+    for code in codes:
+        if code == '49gk6oeq':
+            continue
+        print(f"Processing song ID: {code}")
+        series = get_tiktok_series(code)[1]
+        name = get_tiktok_series(code)[0]
+        artist = get_tiktok_series(code)[2]
+        if series is None:
+            print(f"No data found for {code}.")
+            continue
+        if len(series) > 0:
+            filename = f"tiktok_series_{code}.csv"
+          # write_csv(filename, series)
+            with open(filename, "w", newline="") as file:
+                file.write(name + "\n")
+                file.write(artist + "\n")
+                writer = csv.writer(file)
+                writer.writerows(series)
+            print(f"Saved Tiktok series data for {code} to {filename}")
+        else:
+            print(f"Not enough series available for song ID {code}.")
+
+if __name__ == "__main__":
+    main()
+
+
 
