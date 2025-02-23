174b7ce439894146c858169da2cdbd33e1ba245b
Zhenyu Yu <zhenyuyu@umail.ucsb.edu>
2025-02-23 14:51:59 -0800

    changed get series to read from csv files if possible, then fetch if not found

--- src/utils/song_stats.py
+++ src/utils/song_stats.py
@@ -1,50 +1,57 @@
 import requests
-import pandas
+import pandas as pd
+import os
 
 def get_spotify_playlist_series(song_id: str):
-    res = requests.get(f"https://data.songstats.com/api/v1/analytics_track/{song_id}/top?source=spotify")
+    csv_path = f"../../spotify_playlists_dataset/spotify_playlist_series_{song_id}.csv"
+    if os.path.exists(csv_path):
+        df = pd.read_csv(csv_path)
+        track_name = df['track_name'].iloc[0]
+        last_90_data = df[['timestamp', 'value']].values.tolist()
+        return track_name, last_90_data
 
+    res = requests.get(f"https://data.songstats.com/api/v1/analytics_track/{song_id}/top?source=spotify")
     if res.status_code != 200:
         return None
-    
     parsed_data = res.json()
-
     if parsed_data['result'] == 'success':
         track_name = parsed_data['trackInfo']['trackName']
-        artist_name = parsed_data['trackInfo']['artistName']
-        avatar = parsed_data['trackInfo']['avatar']
         last_90_data = parsed_data['chart']['seriesData'][0]['data'][-90:]
-        return track_name, last_90_data, artist_name, avatar
+        return track_name, last_90_data
 
 def get_spotify_reach_series(song_id: str):
+    csv_path = f"../../spotify_reach_dataset/spotify_reach_series_{song_id}.csv"
+    if os.path.exists(csv_path):
+        df = pd.read_csv(csv_path)
+        track_name = df['track_name'].iloc[0]
+        last_90_data = df[['timestamp', 'value']].values.tolist()
+        return track_name, last_90_data
+
     res = requests.get(f"https://data.songstats.com/api/v1/analytics_track/{song_id}/top?source=spotify")
-
     if res.status_code != 200:
         return None
-    
     parsed_data = res.json()
-
     if parsed_data['result'] == 'success':
         track_name = parsed_data['trackInfo']['trackName']
-        artist_name = parsed_data['trackInfo']['artistName']
-        avatar = parsed_data['trackInfo']['avatar']
         last_90_data = parsed_data['chart']['seriesData'][1]['data'][-90:]
-        return track_name, last_90_data, artist_name, avatar
-    
-def get_tiktok_series(song_id: str):
-    res = requests.get(f"https://data.songstats.com/api/v1/analytics_track/{song_id}/top?source=tiktok")
+        return track_name, last_90_data
 
+def get_tiktok_series(song_id: str):
+    csv_path = f"../../tiktok_series_dataset/tiktok_series_{song_id}.csv"
+    if os.path.exists(csv_path):
+        df = pd.read_csv(csv_path)
+        track_name = df['track_name'].iloc[0]
+        last_90_data = df[['timestamp', 'value']].values.tolist()
+        return track_name, last_90_data
+
+    res = requests.get(f"https://data.songstats.com/api/v1/analytics_track/{song_id}/top?source=tiktok")
     if res.status_code != 200:
         return None
-    
     parsed_data = res.json()
-
     if parsed_data['result'] == 'success':
         track_name = parsed_data['trackInfo']['trackName']
-        artist_name = parsed_data['trackInfo']['artistName']
-        avatar = parsed_data['trackInfo']['avatar']
         last_90_data = parsed_data['chart']['seriesData'][0]['data'][-90:]
-        return track_name, last_90_data, artist_name, avatar
+        return track_name, last_90_data
 
 def get_youtube_series(song_id: str): # video views
     res = requests.get(f"https://data.songstats.com/api/v1/analytics_track/{song_id}/top?source=youtube")
